9be684ee0a335faf8023e6d5a1101433a250e737
Shahid <shahid.alam57@gmail.com>
2022-07-12 19:35:40 +0530

    added about me

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import profilepic from '../images/my-site-pic.png'
 const About = () => {
   return (
-    <div>
-        
+    <div className='mt-4 flex flex-col min-h-screen justify-around'>
+        <h2 className='text-5xl text-center'>im the IT guy your manager sends</h2>
+        <div className='flex justify-center'>
+        <img src={profilepic} alt="cool-profile-pic" className='w-[100%]' />
+        </div>
+        <p>
+          Md Shahid Alam did a minor course in Java when he was 13 and since then was fascinated by the world of technology. Despite being unable to afford a PC, he learnt the basics using pen and paper till he got his first Mac in his sophomore year.
+          <br />
+          <br />
+          He is currently working at BizPilot Financial Pvt Ltd as a Software Developer and help the organization in backend, front end and DevOps.
+          <br />
+          <br />
+          In 2022, he founded <strong>Server[200]Success</strong>, a discord server dedicated to help beginners create and contribute to open-source projects.
+          The aim of the server is to equip aspiring developers with the essential skills and apply them in real world projects, something the author wanted when he first strated out.
+          <br />
+          <br />
+          He is currently pursing Bachelors in Computer Applications and is set to graduate in 2022.
+        </p>
     </div>
   )
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <span className="text-[#08ff17]">results</span>
       </h1>
       <section className="leading-relaxed text-xl p-1 flex flex-col md:flex-row md:justify-between w-full">
-      <img src="https://media.giphy.com/media/jOmQmJkjcvB3Bc8CRb/giphy.gif" alt="" className="mt-4 text-center" />
+      <img src="https://media.giphy.com/media/jOmQmJkjcvB3Bc8CRb/giphy.gif" alt="" className="mt-4 text-center md:w-[15rem]" />
       <p className="md:ml-8">
         <br />
         Hello!👋 
