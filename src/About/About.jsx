5ee4211af87f25c4ed66fe8b9c3b61aa3173d132
Shahid <shahid.alam57@gmail.com>
2022-07-12 19:43:56 +0530

    formatted about us page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import profilepic from '../images/my-site-pic.png'
 const About = () => {
   return (
-    <div className='mt-4 flex flex-col min-h-screen justify-around'>
+    <div className='mt-4 flex flex-col min-h-screen h-[60rem] justify-around'>
         <h2 className='text-5xl text-center'>im the IT guy your manager sends</h2>
         <div className='flex justify-center'>
-        <img src={profilepic} alt="cool-profile-pic" className='w-[100%]' />
+        <img src={profilepic} alt="cool-profile-pic" className='w-[100%] md:w-[60%] lg:w-[40%]' />
         </div>
-        <p>
+        <p className='border border-white p-8'>
           Md Shahid Alam did a minor course in Java when he was 13 and since then was fascinated by the world of technology. Despite being unable to afford a PC, he learnt the basics using pen and paper till he got his first Mac in his sophomore year.
           <br />
           <br />
