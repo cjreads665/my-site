f58c84726ff110d7ab47b007e25695ef5076d33b
Shahid <shahid.alam57@gmail.com>
2022-07-13 16:59:09 +0530

    added content in projects

--- src/data.jsx
+++ src/data.jsx
@@ -1,81 +1,144 @@
 const top = [
-    {
-      name: "soft.tech",
-      description:
-        "A full fledged blog application that allows user to view, like, comment, sign up and much more. User can login to their account and change details such as bio, name,password and email. An account will allow the user to favourite articles. It' also allow users to post their own articles ",
-      glink: "https://github.com/ReallyCoolProjects/soft.dev",
-      dlink: "https://github.com/ReallyCoolProjects/soft.dev",
-      tech: [
-        "reactjs",
-        "nodejs",
-        "taiwlind",
-        "redux",
-        "typescript",
-        "expressjs",
-        "prisma",
-      ],
-    },
-    {
-      name: "Distance Mapper",
-      description:
-        "Distance-Mapper is a tool which helps in calculating distance between two locations. A route between the places is shown on the map.",
+  {
+    name: "soft.tech",
+    description:
+      "A full fledged blog application that allows user to view, like, comment, sign up and much more. User can login to their account and change details such as bio, name,password and email. An account will allow the user to favourite articles. It' also allow users to post their own articles ",
+    glink: "https://github.com/ReallyCoolProjects/soft.dev",
+    dlink: "https://github.com/ReallyCoolProjects/soft.dev",
+    tech: [
+      "reactjs",
+      "nodejs",
+      "taiwlind",
+      "redux",
+      "typescript",
+      "expressjs",
+      "prisma",
+    ],
+  },
+  {
+    name: "Distance Mapper",
+    description:
+      "Distance-Mapper is a tool which helps in calculating distance between two locations. A route between the places is shown on the map.",
+    glink: "https://github.com/cjreads665/distance-mapper",
+    dlink: "https://distance-mapper.netlify.app/",
+    tech: ["reactjs", "mapbox api", "tailwind", "axios"],
+  },
+  {
+    name: "TrakTrack",
+    description:
+      "A web application to track cryptocurrency prices and get their current market value. Clicking on the individual coin will fetch its information along with the price trend of the said coin.",
+    glink: "https://github.com/cjreads665/crypto-web",
+    dlink: "https://tracktrak.netlify.app//",
+    tech: ["reactjs", "coingecko api", "tailwind", "axios"],
+  },
+];
 
-      glink: "https://github.com/cjreads665/distance-mapper",
-      dlink: "https://distance-mapper.netlify.app/",
-      tech: ["reactjs", "mapbox api", "tailwind", "axios"],
-    },
-    {
-      name: "TrakTrack",
-      description:
-        "A web application to track cryptocurrency prices and get their current market value. Clicking on the individual coin will fetch its information along with the price trend of the said coin.",
+const allProjects = [
+  {
+    name: "soft.tech",
+    description:
+      "A full fledged blog application that allows user to view, like, comment, sign up and much more. User can login to their account and change details such as bio, name,password and email. An account will allow the user to favourite articles. It' also allow users to post their own articles ",
+    glink: "https://github.com/ReallyCoolProjects/soft.dev",
+    dlink: "https://github.com/ReallyCoolProjects/soft.dev",
+    tech: [
+      "reactjs",
+      "nodejs",
+      "taiwlind",
+      "redux",
+      "typescript",
+      "expressjs",
+      "prisma",
+      'react-router'
+    ],
+  },
+  {
+    name: "Distance Mapper",
+    description:
+      "Distance-Mapper is a tool which helps in calculating distance between two locations. A route between the places is shown on the map.",
+    glink: "https://github.com/cjreads665/distance-mapper",
+    dlink: "https://distance-mapper.netlify.app/",
+    tech: ["reactjs", "mapbox api", "tailwind", "axios"],
+  },
+  {
+    name: "TrakTrack",
+    description:
+      "A web application to track cryptocurrency prices and get their current market value. Clicking on the individual coin will fetch its information along with the price trend of the said coin.",
+    glink: "https://github.com/cjreads665/crypto-web",
+    dlink: "https://tracktrak.netlify.app//",
+    tech: ["reactjs", "coingecko api", "tailwind", "axios"],
+  },
+  {
+    name: "CodingPal",
+    description:
+      "A discord bot that uses different APIs to fetch data such as cryptocurrency price, news, weather reports,etc.",
+    glink: "https://github.com/ReallyCoolProjects/DiscBot",
+    dlink: "https://github.com/ReallyCoolProjects/DiscBot",
+    tech: ["nodejs", "coingecko api", "discord", "axios", "weather api"],
+  },
+  {
+    name: "City Finder",
+    description:
+      "A website to get the state and city of a particular place in India.",
+    glink: "https://github.com/ReallyCoolProjects/DiscBot",
+    dlink: "https://github.com/ReallyCoolProjects/DiscBot",
+    tech: ["HTML", "CSS", "JAVASCRIPT", "API"],
+  },
+  {
+    name: "Pattern Pattern",
+    description:
+      "A canvas application where a user can swipe (touchscreen) or drag their mouse to draw different patterns on screen.",
+    glink: "https://github.com/cjreads665/creative-draw",
+    dlink: "https://pattern-pattern.netlify.app/",
+    tech: ["html", 'css', 'canvas', 'javascript'],
+  },
+  {
+    name: "OverPay UI",
+    description:
+      "A concept payments Single Page Application with login and sign up pages only.",
+    glink: "https://github.com/cjreads665/overpay-ui",
+    dlink: "http://overpay-ui.vercel.app/",
+    tech: ["reactjs", 'tailwind', 'font awesome', 'react-router'],
+  },
+  {
+    name: "Typing Master",
+    description:
+      "A web application that outputs the user's typing speed. A paragraph and timelimit is given.",
+    glink: "https://github.com/cjreads665/typing-master",
+    dlink: "https://typing-master-js.netlify.app/",
+    tech: ["html", 'css', 'javascript'],
+  },
+  {
+    name: "Video Player",
+    description:
+      "A video player containing a playlist of memes.",
+    glink: "https://github.com/cjreads665/vid-player",
+    dlink: "https://meymey.netlify.app/",
+    tech: ["html", 'css', 'javascript'],
+  },
+  {
+    name: "Meditation Timer",
+    description:
+      "A timer made for taking a break and relax or meditating.",
+    glink: "https://github.com/cjreads665/timer-app",
+    dlink: "https://timer-cj.netlify.app/",
+    tech: ["html", 'css', 'javascript'],
+  },
+  {
+    name: "Drum Kit",
+    description:
+      "A drum kit simulator made using HTML, CSS and Vanilla Javascript. ",
+    glink: "https://github.com/cjreads665/js-drum-kit",
+    dlink: "https://drummer-js.netlify.app/",
+    tech: ["html", 'css', 'javascript'],
+  },
+  {
+    name: "CSS Clock",
+    description:
+      "A CSS clock made using CSS and vanilla javascript.",
+    glink: "https://github.com/cjreads665/clock-js",
+    dlink: "https://clock-js69.netlify.app/",
+    tech: ["html", 'css', 'javascript'],
+  },
+];
 
-
-      glink: "https://github.com/cjreads665/crypto-web",
-      dlink: "https://tracktrak.netlify.app//",
-      tech: ["reactjs", "coingecko api", "tailwind", "axios"],
-    },
-  ];
-
-
-  const allProjects =[
-    {
-        name: "soft.tech",
-        description:
-          "A full fledged blog application that allows user to view, like, comment, sign up and much more. User can login to their account and change details such as bio, name,password and email. An account will allow the user to favourite articles. It' also allow users to post their own articles ",
-        glink: "https://github.com/ReallyCoolProjects/soft.dev",
-        dlink: "https://github.com/ReallyCoolProjects/soft.dev",
-        tech: [
-          "reactjs",
-          "nodejs",
-          "taiwlind",
-          "redux",
-          "typescript",
-          "expressjs",
-          "prisma",
-        ],
-      },
-      {
-        name: "Distance Mapper",
-        description:
-          "Distance-Mapper is a tool which helps in calculating distance between two locations. A route between the places is shown on the map.",
-  
-        glink: "https://github.com/cjreads665/distance-mapper",
-        dlink: "https://distance-mapper.netlify.app/",
-        tech: ["reactjs", "mapbox api", "tailwind", "axios"],
-      },
-      {
-        name: "TrakTrack",
-        description:
-          "A web application to track cryptocurrency prices and get their current market value. Clicking on the individual coin will fetch its information along with the price trend of the said coin.",
-  
-  
-        glink: "https://github.com/cjreads665/crypto-web",
-        dlink: "https://tracktrak.netlify.app//",
-        tech: ["reactjs", "coingecko api", "tailwind", "axios"],
-      },
-  ]
-
-
-
-
-  export {top, allProjects}
+export { top, allProjects };
